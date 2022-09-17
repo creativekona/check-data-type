749cc2b11685dc736f86e9046bee4da6114c8076
Rakesh Kumar <creativekona@gmail.com>
2022-09-17 12:25:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Get type</title>
+	</head>
+	<body>
+		<form method="post">
+			<input type="string" name="input">
+			<button type="submit">Check type</button>
+			<h1>Get data type:</h1>
+			<div>
+				<?php 
+					if(isset($_POST['input'])){
+						echo gettype($_POST['input']);
+					}
+				?>
+			</div>
+		</form>
+	</body>
+</html>
